f3fe753fcf445c67e6a7ce18f55ccaa8efe36957
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-19 19:35:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
-      supplies.include?("BBQ")
+      if supplies.include?("BBQ")
       holiday
   end
 end
